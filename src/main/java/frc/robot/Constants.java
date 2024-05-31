98d08ad4f6411d1e739d0f1e4b10a6ff4c983d9e
Alexander Ament <118925546+Agent-AA@users.noreply.github.com>
2024-05-31 13:39:02 -0400

    Feat-leds
    * Create LedSubsystem for CANdle LEDs
    
    * correct led number

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -125,6 +125,9 @@ public final class Constants {
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
     public static final double kDriveDeadband = 0.05;
+
+    public static final int kCANdleId = 17;
+    public static final int kNumLeds = 128;
   }
 
   public static final class AutoConstants {
