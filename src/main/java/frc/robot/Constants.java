7d44cb3e09e750df47643bf79b8249062723c65b
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-12-06 15:22:53 -0500

    add Arducam name to Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -265,4 +265,8 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+  public static final class VisionConstants {
+    public static final String VISION_CAMERA_NAME = "Arducam_OV2311_USB_Camera";
+  }
 }
