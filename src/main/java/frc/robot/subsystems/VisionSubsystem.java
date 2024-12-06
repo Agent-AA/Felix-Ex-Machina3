cfa7019e25dbbf0164c031ef7bc2882f327dacf1
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-12-06 16:05:33 -0500

    initialize visionSubsystem

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * The VisionSubsystem is one of the more complex robot subsystems, and unlike the others,
+ * exists without any commands.
+ */
+public class VisionSubsystem extends SubsystemBase{
+    
+}
