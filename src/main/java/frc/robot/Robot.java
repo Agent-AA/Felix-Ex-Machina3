338a78308409f723a89fe075c6418848345926eb
SIHS-Robotics <sihsrobotics@ignatius.edu>
2024-12-11 15:40:45 -0500

    Fix: accidentally deleted RobotContainer instance in commit 70627c611d8ff0cdeb277632a43875d7057303f3

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
     m_robotLEDs = RobotContainer.m_robotLEDs;
 
     // Set the LEDs to red for disabled
